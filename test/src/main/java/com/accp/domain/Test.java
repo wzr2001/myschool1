c36a8291861e57b2a8fe2314d85fbb57b98c8b59
Administrator <Administrator@DESK-20180805BH>
2018-11-15 14:26:20 +0800

    这是我的第一次使用gitbht

---
+++ test/src/main/java/com/accp/domain/Test.java
@@ -0,0 +1,8 @@
+package com.accp.domain;
+
+public class Test {
+	//https://github.com/wzr2001/myschool1.git
+	public static void main(String[] args) {
+		System.out.println("nihao>>>>>>>>>>>");
+	}
+}
